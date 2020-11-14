204edcf224b05179e4b2b8cbf9c749cead36cb09
iiiieo <achtbyte01@gmail.com>
2020-11-14 12:51:24 +0100

    com

--- server.js
+++ server.js
@@ -33,28 +33,19 @@ io.on('connection', (socket) => {
   //helper
   socket.on('pressKey', data => {
     //data--> {socketID: "xyz", keyEvent: 61}
-    let targetClient = getTargetClient(data.socketID)
+    let targetClient = connections[data.socketID]
     if(targetClient){
       targetClient.socket.emit('pressKey', data.keyEvent)
     }
   })
   socket.on('moveMouse', data =>{
-    //data--> {socketID: "xyz", mouseX: 615, mouseY: 615}
-    let targetClient = getTargetClient(data.socketID)
+    //data--> {socketId: "xyz", mouseX: 615, mouseY: 615}
+    let targetClient = connections[data.socketId]
     if(targetClient){
       targetClient.socket.emit('moveMouse', { mouseX: data.mouseX, mouseY: data.mouseY})
     }
   })
 });
-
-const getTargetClient = (id) => {
-  let targetClient = connections[id]
-  if(targetClient){
-    socket.emit('log', "KeyEvent sent!")
-  }else{
-    socket.emit('log', "KeyEvent failed! Socket not available")
-  }
-}
 
 http.listen(3000, () => {
   console.log('listening on port 3000');
